b7ae9eb9018578c5b5bfa52939ba8ca36d0f3866
Ryan Spicer <ryanspicer@spicetop.mobile.asu.edu>
2010-09-03 11:36:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmesaVote::Application.config.secret_token = '8375803626dde161edff27ef6e320d76461c1b1705829ec91a001f7115961371a5d2fe5ec6657938a979f140decf111f81328db4b45cabfb462d134ca82de639'
